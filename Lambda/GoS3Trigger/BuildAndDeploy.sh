7c5d52b146e6cee7aa62da63ccbbefea6b8f6e14
Alexandre Kovalenko <sunny@rabbitsden.no-ip.org>
2021-04-30 16:05:35 -0400

    Made sure that AWS CLI does not paginate and added forcing of the bucket removal

--- Lambda/GoS3Trigger/BuildAndDeploy.sh
+++ Lambda/GoS3Trigger/BuildAndDeploy.sh
@@ -14,11 +14,14 @@ then
 fi
 
 #####################################################################################################
-# Please adjust to match your configuration 
 # NOTE: code execution role assumed to exist
 export AWS_REGION=$1
 export TRIGGER_BUCKET_NAME=$2
 export CODE_EXECUTION_ROLE=$3
+
+#####################################################################################################
+# Custom AWS CLI options go in here
+export AWS_CLI_OPTIONS="--no-cli-pager"
 
 #####################################################################################################
 # Fetching necessary bits and building the trigger
@@ -32,17 +35,17 @@ go get github.com/aws/aws-lambda-go/events && \
 
 ######################################################################################################
 # Deleting and recreating the function
-aws lambda delete-function --function-name GoS3Trigger
-aws lambda create-function --function-name GoS3Trigger --runtime go1.x --zip-file fileb://GoS3Trigger.zip --handler GoS3Trigger --role ${CODE_EXECUTION_ROLE} > FunctionDescription.txt || die "Could not create function"
+aws ${AWS_CLI_OPTIONS} lambda delete-function --function-name GoS3Trigger
+aws ${AWS_CLI_OPTIONS} lambda create-function --function-name GoS3Trigger --runtime go1.x --zip-file fileb://GoS3Trigger.zip --handler GoS3Trigger --role ${CODE_EXECUTION_ROLE} > FunctionDescription.txt || die "Could not create function"
 
 ######################################################################################################
 # Deleting and recreating the bucket
-aws s3api delete-bucket --bucket ${TRIGGER_BUCKET_NAME}
-aws s3api create-bucket --bucket ${TRIGGER_BUCKET_NAME} --region ${AWS_REGION} --create-bucket-configuration LocationConstraint=${AWS_REGION} || die "Could not create bucket"
+aws ${AWS_CLI_OPTIONS} s3 rb s3://${TRIGGER_BUCKET_NAME} --force
+aws ${AWS_CLI_OPTIONS} s3api create-bucket --bucket ${TRIGGER_BUCKET_NAME} --region ${AWS_REGION} --create-bucket-configuration LocationConstraint=${AWS_REGION} || die "Could not create bucket"
 
 ######################################################################################################
 # Granting bucket rights to call the trigger
-aws lambda add-permission --function-name GoS3Trigger --action "lambda:InvokeFunction" --source-arn arn:aws:s3:::${TRIGGER_BUCKET_NAME} --statement-id s3invoke --principal s3.amazonaws.com || die "Could not give bucket permission to execute lambdas"
+aws ${AWS_CLI_OPTIONS} lambda add-permission --function-name GoS3Trigger --action "lambda:InvokeFunction" --source-arn arn:aws:s3:::${TRIGGER_BUCKET_NAME} --statement-id s3invoke --principal s3.amazonaws.com || die "Could not give bucket permission to execute lambdas"
 export FUNCTION_ARN=`grep FunctionArn FunctionDescription.txt | tr -d ' ",' | cut -f2- -d:`
 cat <<EOF > notification.configuration
 {
@@ -57,6 +60,6 @@ cat <<EOF > notification.configuration
   ]
 }
 EOF
-aws s3api put-bucket-notification-configuration --bucket ${TRIGGER_BUCKET_NAME} --notification-configuration file://notification.configuration || die "Could not associate notification with the lambda"
+aws ${AWS_CLI_OPTIONS} s3api put-bucket-notification-configuration --bucket ${TRIGGER_BUCKET_NAME} --notification-configuration file://notification.configuration || die "Could not associate notification with the lambda"
 
 rm -f FunctionDescription.txt notification.configuration
