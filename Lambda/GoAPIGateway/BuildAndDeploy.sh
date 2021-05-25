be87e5455c2546b03d19639253840229590cc2b6
Alexandre "Sunny" Kovalenko <alexandre.kovalenko@outlook.com>
2021-05-25 21:46:50 +0000

    Internediate commit of the build and deploy script

--- Lambda/GoAPIGateway/BuildAndDeploy.sh
+++ Lambda/GoAPIGateway/BuildAndDeploy.sh
@@ -35,7 +35,25 @@ go get github.com/aws/aws-lambda-go/events && \
 ######################################################################################################
 # Deleting and recreating the function
 aws ${AWS_CLI_OPTIONS} lambda delete-function --function-name GoAPIGateway
-aws ${AWS_CLI_OPTIONS} lambda create-function --function-name GoAPIGateway --runtime go1.x --zip-file fileb://GoAPIGateway.zip --handler GoAPIGateway --role ${CODE_EXECUTION_ROLE} > FunctionDescription.txt || die "Could not create function"
+aws ${AWS_CLI_OPTIONS} lambda create-function --function-name GoAPIGateway --runtime go1.x --zip-file fileb://GoAPIGateway.zip --handler GoAPIGateway --role ${CODE_EXECUTION_ROLE} | tee functiondescription.txt || die "Could not create function"
+export FUNCTION_ARN=`egrep '^[ \t]+"FunctionArn"[ ]*:[ ]*"[^"]+",[ ]*$' functiondescription.txt | cut -f4 -d\"`
+
+#######################################################################################################
+# Dropping and recreating API
+export API_ID=`aws apigateway get-rest-apis --query "items[?name==\\\`GoAPIGateway\\\`]" | egrep '^[ \t]+"id"[ ]*:[ ]*"[^"]+",[ ]*$' | cut -f4 -d\"`
+if [ "X${API_ID}" != X ]
+then
+  aws apigateway delete-rest-api --rest-api-id ${API_ID} || die "Could not delete API"
+fi
+aws apigateway create-rest-api --name GoAPIGateway --description "Simple API gateway for Go Lambda" --endpoint-configuration types=REGIONAL --region ${AWS_REGION} | tee apidescription.txt || die "Could not create API"
+export API_ID=`egrep '^[ \t]+"id"[ ]*:[ ]*"[^"]+",[ ]*$' apidescription.txt | cut -f4 -d\"`
+aws apigateway get-resources --rest-api-id ${API_ID} | tee parentresource.txt || die "Cannot get parent resource for the API"
+export PARENT_RESOURCE_ID=`egrep '^[ \t]+"id"[ ]*:[ ]*"[^"]+",[ ]*$' parentresource.txt | cut -f4 -d\"`
+aws apigateway create-resource --rest-api-id ${API_ID} --parent-id ${PARENT_RESOURCE_ID} --path-part "{proxy+}" --region ${AWS_REGION} | tee resource.txt || die "Cannot create resource for API"
+export RESOURCE_ID=`egrep '^[ \t]+"id"[ ]*:[ ]*"[^"]+",[ ]*$' resource.txt | cut -f4 -d\"`
+aws apigateway put-method  --rest-api-id ${API_ID}  --resource-id ${RESOURCE_ID} --http-method GET --authorization-type NONE --no-api-key-required | tee method.txt || die "Cannot create method for API"
+aws apigateway put-integration --rest-api-id ${API_ID} --resource-id ${RESOURCE_ID} --http-method GET --type AWS_PROXY --integration-http-method GET --uri arn:aws:apigateway:${AWS_REGION}:lambda:path/2015-03-31/functions/${FUNCTION_ARN}/invocations --region ${AWS_REGION} || die "Cannot add integration to API"
+aws lambda add-permission --function-name ${FUNCTION_ARN} --statement-id invoke_${API_ID} --action lambda:InvokeFunction --principal apigateway.amazonaws.com || die "Could not grant API gateway permission to invoke Go Lambda"
 
 exit 0
 
