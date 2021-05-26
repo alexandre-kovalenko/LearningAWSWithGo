ad46cf07ceaa2ae9f5a027b62170270f5ae38a3a
Alexandre "Sunny" Kovalenko <alexandre.kovalenko@outlook.com>
2021-05-26 15:00:26 -0400

    Made sure that update script is using region parameter

--- Lambda/GoAPIGateway/UpdateAndDeploy.sh
+++ Lambda/GoAPIGateway/UpdateAndDeploy.sh
@@ -1,5 +1,21 @@
-#!/bin/ksh
+#!/bin/sh
 
+die() {
+  echo $*
+  exit -1
+}
+
+if [ $# != 1 ] 
+then
+  die Usage: UpdateAndDeploy.sh "<AWS region>"
+fi
+
+#####################################################################################################
+export AWS_REGION=$1
+export AWS_CLI_OPTIONS="--no-cli-pager --region ${AWS_REGION}"
+export AWS="aws ${AWS_CLI_OPTIONS}"
+
+#####################################################################################################
 go build GoAPIGateway.go
 zip GoAPIGateway.zip GoAPIGateway
-aws lambda update-function-code --function GoAPIGateway --zip-file fileb://GoAPIGateway.zip
+${AWS} lambda update-function-code --function GoAPIGateway --zip-file fileb://GoAPIGateway.zip
